a97e77d29da81879b465498e7086cd1192d5082d
Leandro Diaz <leandroAdiazR@gmail.com>
2021-09-06 12:22:05 -0400

    Initial Commit

---
+++ ChatUI/ChatUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUIApp.swift
+//  ChatUI
+//
+//  Created by Leandro A diaz R on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
